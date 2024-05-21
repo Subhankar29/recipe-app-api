8530051bbb4504b8fe9108277326f89f4768989d
subhankar shah <subhankar29shah@gmail.com>
2024-05-21 14:10:42 -0400

    Added support for Postgresql

---
+++ Recipe-App/app/app/test/test_calculator.py
@@ -0,0 +1,14 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+class ClacTests(SimpleTestCase):
+    """
+    Test the adding numbers together.
+    """
+
+    def test_add_numbers(self):
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11)
+    
